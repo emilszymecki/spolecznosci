ad9a4689e4831f31ea5a70aa675c8e4f76455e23
Emil <emilszymecki@gmail.com>
2019-02-07 23:03:13 +0100

    Update relative_click_viewPort.js

--- relative_click_viewPort.js
+++ relative_click_viewPort.js
@@ -1,1 +1,3 @@
 $(document).click(e => console.log(e.pageY,$(document).height(),`${Math.round(e.pageY * 100 / $(document).height() )}%` ))
+//Vanilla
+document.addEventListener("click", e => console.log(e.pageY,document.body.scrollHeight,`${Math.round(e.pageY*100/document.body.scrollHeight)}%`))
