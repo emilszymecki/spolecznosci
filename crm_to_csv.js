f4d532ac54898500f70993e54d5dd44697cfeaa6
Emil <emilszymecki@gmail.com>
2019-02-12 16:39:26 +0100

    Update crm_to_csv.js

--- crm_to_csv.js
+++ crm_to_csv.js
@@ -19,7 +19,7 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
 	for (var i = 0; i < arrData.length; i++) {
 		var row = "";
 		for (var index in arrData[i]) {
-			row += '"' + arrData[i][index] + '",';
+			row += '' + arrData[i][index] + ',';
 		}
 		row.slice(0, row.length - 1);
 		CSV += row + "\r\n";
@@ -38,4 +38,4 @@ function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
 	link.click();
 }
 
-JSONToCSVConvertor(JSON.stringify(table),"DUPA2",true)
+JSONToCSVConvertor(JSON.stringify(table),"DUPA_x2",true)
