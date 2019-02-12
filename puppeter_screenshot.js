0978154bc991d460df3020837112c7afd777dc53
Emil <emilszymecki@gmail.com>
2019-02-12 16:51:42 +0100

    Update puppeter_screenshot.js

--- puppeter_screenshot.js
+++ puppeter_screenshot.js
@@ -1,37 +1,60 @@
 const puppeteer = require('puppeteer');
-
+const devices = require('puppeteer/DeviceDescriptors');
+const path = require('path');
+const { PendingXHR } = require('pending-xhr-puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless:true,ignoreHTTPSErrors: true,args: ['--start-maximized']});
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1680, height: 1050 })
-  await page.goto('https://jbzdy.pl/main',{waitUntil: 'networkidle2'});
   
+   const browser = await puppeteer.launch({headless:true,ignoreHTTPSErrors: true,args: ['--start-maximized']});
+
+  for(let loop in emulatedDevices){
+    console.log(emulatedDevices[loop])
+    const page = await browser.newPage();
+    const pendingXHR = new PendingXHR(page);
+    //await page.setViewport({ width: 1680, height: 1050 })
+    await page.emulate(emulatedDevices[loop]);
+    await page.goto('https://jbzdy.pl'/*'https://kwejk.pl'*/,{waitUntil: 'networkidle2'});
+    
 
 
-  // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
+    // Get the "viewport" of the page, as reported by the page.
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
+      };
+    });
+
+    try {
+      await page.waitForSelector('.intro_acceptAll--SPx18r')
+      await page.click('.intro_acceptAll--SPx18r');
+    } catch (error) {
+      console.log("The element didn't appear.")
+    }
 
 
-  await page.waitFor(60000)
-  await autoScroll(page);
-  
-  await page.evaluate( () => {
-  	document.querySelectorAll('[id^="spolecznosci"]').forEach(x => x.style.border='2px solid #E8272C')
-  	//return items
-  })
+    await page.waitFor(15000)
+    await autoScroll(page);
+    await autoScrollReverse(page)
 
-  await page.screenshot({path: 'screenshot.png',fullPage: true});
+    await Promise.race([
+      pendingXHR.waitForAllXhrFinished(),
+      new Promise(resolve => {
+          setTimeout(resolve, 100000);
+      }),
+  ]);
 
-  console.log('Dimensions:', dimensions);
-  //console.log(ads)
+    
+    await page.evaluate( () => {
+      document.querySelectorAll('[id^="spolecznosci"]').forEach(x => x.style.border='2px solid #E8272C')
+      //return items
+    })
 
+    await page.screenshot({path: `${emulatedDevices[loop].name}-scr.png`,fullPage: true});
+
+    console.log('Dimensions:', dimensions);
+  }
   await browser.close();
 })();
 
@@ -40,18 +63,58 @@ async function autoScroll(page){
     await page.evaluate(async () => {
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
-            var distance = 100;
+            var distance = 200;
             var timer = setInterval(() => {
                 var scrollHeight = document.body.scrollHeight;
                 window.scrollBy(0, distance);
                 totalHeight += distance;
                 if(totalHeight >= scrollHeight){
-                	//window.scrollBy(0, 0);
+                	  //window.scrollBy(0,-(document.body.scrollHeight))
                     clearInterval(timer);
                     resolve();
                 }
-            }, 500);
+            }, 400);
         });
     });
 }
 
+async function autoScrollReverse(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = document.body.scrollHeight;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = 0;
+                window.scrollBy(0, -distance);
+                totalHeight -= distance;
+                if(totalHeight <= scrollHeight){
+                    //window.scrollBy(0,-(document.body.scrollHeight))
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 400);
+        });
+    });
+}
+
+
+const emulatedDevices = [
+  {
+    name: 'Desktop 1920x1080',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+    viewport: {
+      width: 1920,
+      height: 1080
+    }
+  },
+  {
+    name: 'Desktop 1024x768',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36',
+    viewport: {
+      width: 1024,
+      height: 768
+    }
+  },
+  devices['iPhone X'],
+  devices['Galaxy S5'],
+]
