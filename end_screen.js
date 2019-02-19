2bd76b4ef50b5a94a4529f4e188129345e9a9181
Emil <emilszymecki@gmail.com>
2019-02-19 13:30:38 +0100

    Create end_screen.js

---
+++ end_screen.js
@@ -0,0 +1,179 @@
+const puppeteer = require("puppeteer");
+const devices = require("puppeteer/DeviceDescriptors");
+const { PendingXHR } = require("pending-xhr-puppeteer");
+const fs = require('fs');
+
+(async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		executablePath:  "C:/Users/Administrator/AppData/Local/Chromium/Application/chrome.exe",
+		userDataDir: "C:/Users/Administrator/AppData/Local/Chromium/User Data/Default/",
+		args: ["--start-maximized",'--use-gl=desktop'],
+		ignoreHTTPSErrors: true
+		//args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
+
+	for(let site of sites){
+		for (let loop in emulatedDevices) {
+			const page = await browser.newPage();
+			const pendingXHR = new PendingXHR(page);
+			//await page.setViewport({ width: 1680, height: 1050 })
+			const urls = new URL(site)
+			const iter = sites.indexOf(site)
+			await page.emulate(emulatedDevices[loop]);
+			await page.goto( site , {
+				waitUntil: "networkidle2"
+			});
+
+			// Get the "viewport" of the page, as reported by the page.
+			const dimensions = await page.evaluate(() => {
+				return {
+					width: document.documentElement.clientWidth,
+					height: document.documentElement.clientHeight,
+					deviceScaleFactor: window.devicePixelRatio
+				};
+			});
+
+			await page.waitFor(3000);
+			await autoScroll(page);
+			await autoScrollReverse(page);
+
+			await Promise.race([
+				pendingXHR.waitForAllXhrFinished(),
+				new Promise(resolve => {
+					setTimeout(resolve, 40000);
+				})
+			]);
+
+
+			var placements = await page.evaluate( async () => {
+				/*var all = Array.from(document.querySelectorAll('[id^="spolecznosci"]'))
+				var pos	= all.map( x => x.getBoundingClientRect().top + (window.scrollY / 2))
+				return pos*/
+				var all = Array.from(document.querySelectorAll('[id^="spolecznosci"]'))
+				var pos	= all.map( x => x.id)
+				return pos
+		    })
+
+		    async function screenshotDOMElement(selector, padding = 0) {
+			  const rect = await page.evaluate(selector => {
+			    const element = document.getElementById(selector);
+			   	element.scrollIntoView()
+			   	window.scrollBy(0, -65);
+			    element.getBoundingClientRect()
+			    const {x, y, width, height} = element.getBoundingClientRect();
+			     /*
+			     if(element.children.length){
+
+						const txt = document.createElement('p');
+					    txt.innerHTML = `${selector} size:${width}x${height}`;
+						txt.style.background = "red";
+						txt.style.position = "relative";
+						txt.style.display = "block";
+						//element.style.border='2px solid blue'
+						element.insertBefore(txt, this.firstChild);
+
+			    }*/
+
+			    return {left: x, top: y, width, height, id: element.id, children: element.children.length};
+			  }, selector);
+			  console.log(rect)
+			  if(rect.children !== 0 || (rect.width !== 0 && rect.height !== 0)){
+			  	await page.waitFor(2000);
+			  	await fs.mkdir(`${__dirname}/${urls.host}_${iter}/${emulatedDevices[loop].name}`, { recursive: true }, (err) => {
+				  if (err) throw err;
+				});
+			  	return await page.screenshot({
+			  	  path: `${__dirname}/${urls.host}_${iter}/${emulatedDevices[loop].name}/${urls.host}-${emulatedDevices[loop].name}-${selector}-elm.png`,
+			  	});
+			  }else{
+			  	await page.waitFor(500);
+			  }
+			}
+
+			for(scroller of placements){
+				try{
+				await screenshotDOMElement(scroller)
+				}catch(e){
+					console.log("zonki",e)
+				}
+			}
+
+			await page.screenshot({path: `${urls.host}_${iter}-${emulatedDevices[loop].name}-all.png`,fullPage: true});
+			await page.waitFor(2000);
+			
+			console.log("Dimensions:", dimensions);
+		}
+	}
+
+	await browser.close()
+})();
+
+const sites = [
+	'https://www.fotka.com/profil/barraf?snet={8,195811,8d7c5e905191491ebda44754ca284b77,1}',
+	'https://www.fotka.com/profil/barraf?snet={8,195812,8bbbbc27ebedd9057a1cc514db084686,1}',
+	'https://www.fotka.com/profil/barraf?snet={11,195815,6406a12fe28dbd37c78442be3456dac1,1}',
+	'https://www.fotka.com/profil/barraf?snet={11,195816,dd309c3735909e0911392dc97823f162,1}'
+];
+
+
+async function autoScroll(page) {
+	await page.evaluate(async () => {
+		await new Promise((resolve, reject) => {
+			var totalHeight = 0;
+			var distance = 200;
+			var timer = setInterval(() => {
+				var scrollHeight = document.body.scrollHeight;
+				window.scrollBy(0, distance);
+				totalHeight += distance;
+				if (totalHeight >= scrollHeight) {
+					//window.scrollBy(0,-(document.body.scrollHeight))
+					clearInterval(timer);
+					resolve();
+				}
+			}, 100);
+		});
+	});
+}
+
+async function autoScrollReverse(page) {
+	await page.evaluate(async () => {
+		await new Promise((resolve, reject) => {
+			var totalHeight = document.body.scrollHeight;
+			var distance = 100;
+			var timer = setInterval(() => {
+				var scrollHeight = 0;
+				window.scrollBy(0, -distance);
+				totalHeight -= distance;
+				if (totalHeight <= scrollHeight) {
+					//window.scrollBy(0,-(document.body.scrollHeight))
+					clearInterval(timer);
+					resolve();
+				}
+			}, 100);
+		});
+	});
+}
+
+const emulatedDevices = [
+	{
+		name: "Desktop 1920x1080",
+		userAgent:
+			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36",
+		viewport: {
+			width: 1920,
+			height: 1080
+		}
+	},
+	{
+		name: "Desktop 1024x768",
+		userAgent:
+			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36",
+		viewport: {
+			width: 1024,
+			height: 768
+		}
+	},
+	//devices["iPhone X"],
+	//devices["Galaxy S5"]
+];
