fb06c9790a3fce12c8157c8dadb01a3b2581be93
Emil <emilszymecki@gmail.com>
2019-04-11 16:57:41 +0200

    Update leazy_loading_ads.js

--- leazy_loading_ads.js
+++ leazy_loading_ads.js
@@ -1,25 +1,40 @@
-const myImgs = document.querySelectorAll('.spolecznoscinet');
-
-observer = new IntersectionObserver(entries => {
+var observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-  	const id = entry.target.id
-  	const idnbr = parseInt(id.split("-")[1])
-  	 _qasp.push(['enable', idnbr]);
+	$(entry.target).hide().fadeIn(6000)
+  	var id = entry.target.id
+  	var idnbr = parseInt(id.split("-")[1])
     if (entry.intersectionRatio > 0) {
       console.log('in the view',entry);
-      observer.unobserve(entry.target);
-      //_qasp.push(['enable', idnbr]);
       _qasp.push(['sadInit', ['sadKwejk', idnbr]]);
+      observer.unobserve(entry.target);
     } else {
       console.log('out of view');
     }
   });
 },{
-    rootMargin: '1000px 0px'
+    rootMargin: '100px 0px'
 });
 
+
+function getSiblings(el, filter) {
+    var siblings = [];
+    el = el.parentNode.firstChild;
+    do { if (!filter || filter(el)) siblings.push(el); } while (el = el.nextSibling);
+    return siblings;
+}
+
+var el = document.querySelector('body');
+
+var sibs = getSiblings(el,(el) => el.nodeName.toLowerCase() == 'iframe');
+
+sibs.forEach(elem => elem.parentNode.removeChild(elem))
+
+var myImgs = document.querySelectorAll('.spolecznoscinet');
+
+
 myImgs.forEach(image => {
-  const idnbr =  parseInt(image.id.split("-")[1])
+  var idnbr =  parseInt(image.id.split("-")[1])
   _qasp.push(['disable', idnbr]);
+  _qasp.push(['enable', idnbr]);
   observer.observe(image);
 });
