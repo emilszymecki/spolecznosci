2ecf646fba32ec60e709be24fded0bac849892cc
Emil <emilszymecki@gmail.com>
2019-10-09 15:47:32 +0200

    Create prebidsConcurentReader.js

---
+++ prebidsConcurentReader.js
@@ -0,0 +1,45 @@
+function getKeys() {
+  var temp = [];
+  for (var b in window) {
+    if (window.hasOwnProperty(b) && typeof window[b] == "object") {
+      temp.push([b, window[b]]);
+    }
+  }
+
+  var tempNoNull = temp.filter(
+    x => x[1] !== null && x[1].constructor == Object
+  );
+  var filterOnlyVersion = tempNoNull.filter(x =>
+    x[1].hasOwnProperty("requestBids")
+  );
+
+  return filterOnlyVersion;
+}
+
+function getInfoPrebid([key, prebid] = arr) {
+  return {
+    [key]: {
+      adsUnitLength: prebid.que.length,
+      allBidsPlacement: Object.values(prebid.getBidResponses())
+        .flatMap(x => x.bids)
+        .flatMap(x => ({
+          adUnitCode: x.adUnitCode,
+          bidderCode: x.bidderCode,
+          size: x.size,
+          height: x.height,
+          cpm: x.cpm,
+          currency: x.currency
+        })),
+      highestCpmBids: prebid.getAllWinningBids().map(x => ({
+        adUnitCode: x.adUnitCode,
+        bidderCode: x.bidderCode,
+        size: x.size,
+        height: x.height,
+        cpm: x.cpm,
+        currency: x.currency
+      }))
+    }
+  };
+}
+
+getKeys().map(getInfoPrebid);
