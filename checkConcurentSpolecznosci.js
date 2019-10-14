e6eed129bc46ec7e22449d78f0801eae2ac287bd
Emil <emilszymecki@gmail.com>
2019-10-14 16:10:59 +0200

    onlydirect

--- checkConcurentSpolecznosci.js
+++ checkConcurentSpolecznosci.js
@@ -12,24 +12,43 @@ function searchPartner() {
 		.filter(x => window[x].hasOwnProperty("requestBids"));
 	var gKeysResseler = {
 		spolecznosci: ["ca-pub-2365165112249593"],
-		optad: ["ca-pub-5512390705137507"],
-		yieldBird: [
-			"ca-pub-8241049497608997",
-			"ca-pub-9467340974789471",
-			"ca-pub-0958205548775696",
-			"ca-pub-6512936480753445"
+		yeldbird: [
+			"ca-pub-0206338760093719",
+			"ca-pub-7756348393895452",
+			"ca-pub-4307100594778012",
+			"ca-pub-9914992914910847"
 		],
-		way2grow: [
-			"ca-pub-4910050017906143",
-			"ca-pub-9328633604439863",
-			"ca-pub-3268638132077067",
-			"ca-pub-7683628640306220"
+		wp: [
+			"ca-pub-5995202563537249",
+			"ca-pub-5443361439335742",
+			"ca-pub-6523315197642632",
+			"ca-pub-5599500498136891",
+			"ca-pub-9335149169362677"
 		],
-		onet: ["ca-pub-5786819153313314"],
-		polskaprese: ["ca-pub-4278860704973981"],
-		yieldriser: ["ca-pub-6918582494067064"],
-		adv_media: ["ca-pub-1409765517756851", "ca-pub-4075894099602271"],
-		moneytizer: ["ca-pub-3700011705422030"]
+		waytogrow: ["ca-pub-7235212965293571", "ca-pub-9934065421505939"],
+		onet: [
+			"ca-pub-1641094773980998",
+			"ca-pub-3179454501132977",
+			"ca-pub-4069369533017132",
+			"ca-pub-4314242355228518",
+			"ca-pub-5310645159024207",
+			"ca-pub-7793577536995585",
+			"ca-pub-8329097311586633",
+			"ca-pub-9378634550872293"
+		],
+		interia: ["ca-pub-9136549947088673", "ca-pub-1636271870028934"],
+		polskaprese: ["ca-pub-2469667141331842", "ca-pub-3299786496633692"],
+		yieldlove: ["ca-pub-1274416353693598", "ca-pub-8813347375005258"],
+		advantage_media: [
+			"ca-pub-6396844742497208",
+			"ca-pub-1966721192709606",
+			"ca-pub-2011734697823029",
+			"ca-pub-6644558441501035",
+			"ca-pub-7958959566206860",
+			"ca-pub-4152552776930088"
+		],
+		rtbnow: ["ca-pub-9655409891876729"],
+		yieldriser: ["ca-pub-8655001720710332", "ca-pub-3191894791526522"]
 	};
 	var adDict = {
 		optad: ["optad", "pbjs325474", "pbjs325474hb", "optad360"],
@@ -101,7 +120,7 @@ console.table(
 		way2grow,
 		adkontekst,
 		gTag,
-		prebid,
+		prebid
 	}) => ({
 		...SSP,
 		spolecznosci,
@@ -113,6 +132,6 @@ console.table(
 		way2grow,
 		adkontekst,
 		gTag,
-		...prebid,
+		...prebid
 	}))(searchPartner())
 );
