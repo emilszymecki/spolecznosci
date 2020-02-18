4f1ac9155ea75ee50e9427ea2e32118ca2d82d2a
Emil <emilszymecki@gmail.com>
2020-02-18 11:05:51 +0100

    Update and rename expSpolecznosci2.js to expSpolecznosci3.js

--- expSpolecznosci3.js
+++ expSpolecznosci3.js
@@ -1,7 +1,7 @@
 var createIframe = () => {
     console.log("Run");
     var createIframe = window.createIframe;
-    var coreCreate = (elGen,domain) => {
+    var coreCreate = (elGen,domain,int) => {
     var createEl = document.createElement('iframe');
     var createScriptInit = document.createElement('script');
     createScriptInit.innerHTML = `
@@ -17,17 +17,29 @@ var createIframe = () => {
         _qasp.push('go');
         _qasp.push(['setFastPAID','sad${domain}']);
         initAds(document, 'script', '//s.spolecznosci.net/js/core2-min.js');
+        setInterval(() => {
+            console.log("check");
+            [...document.querySelectorAll('div[class="spolecznoscinet"]')].forEach(x => {
+                if(x.innerHTML.length < 100){
+                     window["_qasp"] = [];
+                    _qasp.push('go');
+                    _qasp.push(['setFastPAID', 'sadConamiescie']);
+                    initAds(document, 'script', '//s.spolecznosci.net/js/core2-min.js');
+                }
+            })
+        },5000);
     `;
     createEl.src = 'about:blank';
+    createEl.id = int;
     document.body.appendChild(createEl);  
    	createEl.contentDocument.write(elGen)
     createEl.contentDocument.body.appendChild(createScriptInit);
     };
 
     var domain = window.location.hostname.replace("www.","").split(".").slice(0,-1).join("").split("").map((x,i) => i==0?x.toUpperCase():x).join("");
-    [...document.querySelectorAll('div[class="spolecznoscinet"]')].forEach(x => {
+    [...document.querySelectorAll('div[class="spolecznoscinet"]')].forEach((x,i) => {
          console.log(x);
-    	 coreCreate(`<div class="spolecznoscinet" id=${x.id}></div>`,domain);
+    	 coreCreate(`<div class="spolecznoscinet" id=${x.id}></div>`,domain,"el"+i);
 	});
    
 }
